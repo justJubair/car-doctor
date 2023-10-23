0028bf54eb15adc2c75b2622c0beba7b22bb94aa
Jubair <jubair.ahmed2838@gmail.com>
2023-10-23 20:50:29 +0600

    Add: show more button functionality added

--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -2,11 +2,11 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import PropTypes from "prop-types"
 const ServiceCard = ({service}) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card bg-base-100 shadow-xl">
   <figure className="px-10 pt-10">
     <img src={service.img} alt="Shoes" className="rounded-xl" />
   </figure>
-  <div className="card-body items-center text-center">
+  <div className="card-body ">
   <h2 className="card-title">{service.title}</h2>
     
     <div className="flex w-full items-center">
