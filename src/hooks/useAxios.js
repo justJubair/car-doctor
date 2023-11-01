0d12c46f78c2868f5f34fda4da4266070bd65888
Jubair <jubair.ahmed2838@gmail.com>
2023-11-02 01:51:54 +0600

    Add: user navigated after token expired

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,24 +1,31 @@
 import axios from "axios"
 import { useEffect } from "react"
 import useAuth from "./useAuth"
-
+import {useNavigate} from "react-router-dom"
 const axiosSecure = axios.create({
     baseURL: "http://localhost:5000",
     withCredentials:true
 })
 const useAxios =()=>{
-    const {logOut} = useAuth()
+    const {logOut} = useAuth();
+    const navigate = useNavigate()
     useEffect(()=>{
         axiosSecure.interceptors.response.use(res=>{
             return res;
         }, error=>{
             console.log("interceptor", error.response.status)
             if(error.response.status === 401 || error.response.status=== 403){
-                return logOut()
+                logOut()
+                .then(()=>{
+                    navigate("/login")
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
             }
             
         })
-    },[logOut])
+    },[logOut,navigate])
     return axiosSecure
 }
 
